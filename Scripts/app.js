592d682d473a44df3e9505bf5fe8bb16f745a3ab
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2016-02-12 21:07:23 -0500

    added basic nav, folder structure, simple js functions

---
+++ Scripts/app.js
@@ -0,0 +1,21 @@
+	function sayHi(){
+		alert('Hi');
+	}
+
+	function changeMe(){
+		var heading = document.getElementById('heading');
+		heading.innerHTML = "No Kidding";
+	}
+
+	function changeMeBack(){
+		var heading = document.getElementById('heading');
+		heading.innerHTML = "This is a Heading";
+	}
+
+	function focusIn(){
+		alert('I am in the field');
+	}
+
+	function blurOut(){
+		alert('I am out of the field');
+	}
